473900e3b2209fe1cf146068d3dfa1f522d022b5
Tomaz Canabrava <tomaz.canabrava@gmail.com>
2016-08-28 20:35:48 -0300

    Added missing include.
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@gmail.com>

--- string-helpers.h
+++ string-helpers.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 std::string camel_case_to_underscore(std::string s);
 std::string underscore_to_camel_case(std::string s);
 std::string capitalize(std::string s, int pos = 0);
