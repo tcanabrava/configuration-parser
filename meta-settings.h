c452c39f79bacdacc0c6fa20b638068cace98a84
Tomaz Canabrava <tomaz.canabrava@gmail.com>
2016-08-28 20:34:08 -0300

    Moved definitions to header.
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@gmail.com>

---
+++ meta-settings.h
@@ -0,0 +1,23 @@
+#pragma once
+
+struct MetaProperty {
+    std::string name;
+    std::string default_value;
+    std::string type;
+};
+
+struct MetaClass {
+        std::vector<std::unique_ptr<MetaProperty>> properties;
+        std::vector<std::unique_ptr<MetaClass>> subclasses;
+        std::string name;
+        MetaClass *parent;
+};
+
+template< typename... T > struct RecursiveHelper {
+    typedef std::function< RecursiveHelper(T...) > type;
+    RecursiveHelper( type f ) : func(f) {}
+    operator type () { return func; }
+    type func;
+};
+
+typedef RecursiveHelper<std::ifstream&>::type callback_t;
