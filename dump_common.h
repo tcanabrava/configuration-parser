7a40b2508229e4f42efd11a95ff484afe45c2e61
Tom Eccles <tom.eccles@codethink.co.uk>
2021-03-18 15:56:00 +0000

    Add a notice at the top of generated files
    The notice explains that they are automatically generated and should not
    be edited in place.

--- dump_common.h
+++ dump_common.h
@@ -10,3 +10,4 @@ void dump_source_set_methods(std::ofstream& f, MetaClass *currClass);
 void dump_header_properties(std::ofstream &file, const std::vector<std::shared_ptr<MetaProperty>> &properties);
 void dump_header_q_properties(std::ofstream& f,  const std::vector<std::shared_ptr<MetaProperty>> &properties);
 void dump_parameter(std::ofstream& file, const std::shared_ptr<MetaProperty>& property);
+void dump_notice(std::ofstream& f);
