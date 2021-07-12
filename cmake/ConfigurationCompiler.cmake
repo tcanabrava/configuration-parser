bf5d581292e80cda5c0a99911e8ee4c1af8ac385
Tomaz Canabrava <tcanabrava@kde.org>
2021-07-12 15:34:28 +0100

    Export usage to cmake

--- cmake/ConfigurationCompiler.cmake
+++ cmake/ConfigurationCompiler.cmake
@@ -1,11 +1,17 @@
 function(compile_configuration outfiles)
     set(options)
-    set(oneValueArgs TARGET)
+    set(oneValueArgs TARGET EXPORT_HEADER)
     set(multiValueArgs OPTIONS DEPENDS)
 
     cmake_parse_arguments(_WRAP_CPP "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     set(configuration_files ${_WRAP_CPP_UNPARSED_ARGUMENTS})
+
+    if (NOT _WRAP_CPP_EXPORT_HEADER STREQUAL "")
+        set(EXPORT_HEADER_USAGE)
+    else()
+        set(EXPORT_HEADER_USAGE "--with-export-header=${_WRAP_CPP_EXPORT_HEADER}")
+    endif()
 
     foreach(it ${configuration_files})
         get_filename_component(it ${it} ABSOLUTE)
@@ -14,7 +20,7 @@ function(compile_configuration outfiles)
 
         add_custom_command(
             OUTPUT "${_FILENAME_}.cpp" "${_FILENAME_}.h"
-            COMMAND confgen ${it}
+            COMMAND confgen ${EXPORT_HEADER_USAGE} ${it}
             WORKING_DIRECTORY ${CMAKE_BUILD_FOLDER}
         )
 
